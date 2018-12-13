40377b501c76625176772a803d757f7ad3a5911e
lmjing <lmjing1234@gmail.com>
2018-12-13 20:21:19 +0900

    이미지 인식시 상단 버튼 표출 되도록

---
+++ Assets/BattleCardWar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleCardWar : MonoBehaviour {
+    public TrackingObject obj_java_book;
+    public TrackingObject obj_arvr_book;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        if (obj_arvr_book.is_detected_)
+        {
+            GUI.Button(new Rect(300, 300, 240, 120), "ARVR 책 인식됨");
+        }
+
+        if (obj_java_book.is_detected_)
+        {
+            GUI.Button(new Rect(600, 300, 240, 120), "JAVA 책 인식됨");
+        }
+    }
+}
---
+++ Assets/TrackingObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TrackingObject : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour trackableBehaviour;
+    public bool is_detected_ = false; 
+
+    // Use this for initialization
+    void Start () {
+        trackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (trackableBehaviour)
+        {
+            // 상태가 변하는 것에 대해 알 수 있도록 등록
+            trackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            is_detected_ = true;
+        }
+        else
+        {
+            is_detected_ = false;
+        }
+        //throw new System.NotImplementedException();
+    }
+}
