952a48213623373e7bca9894b724d604817ac0f8
lmjing <lmjing1234@gmail.com>
2018-12-14 18:33:51 +0900

    되는데 데이터가 안옴

--- Assets/API.cs
+++ Assets/API.cs
@@ -59,81 +59,6 @@ public class API : MonoBehaviour {
         string calorie;
         string name;
     }
-    
-    //public void Request()
-    //{
-    //    //StartCoroutine(Upload());
-
-    //    Login login = new Login();
-    //    login.id = "KSH";
-    //    login.password = "111";
-
-    //    string json = JsonUtility.ToJson(login);
-
-    //    StartCoroutine(Post("http://192.168.0.4:7080/heroes/user/management/login", json));
-
-    //    if (www.isNetworkError || www.isHttpError)
-    //    {
-    //        Debug.Log(www.error);
-    //    }
-    //    else
-    //    {
-    //        // show result as text
-    //        Debug.Log("success!");
-    //        Response response = JsonUtility.FromJson<Response>(www.downloadHandler.text);
-
-    //        responseText.text = response.data.name;
-
-    //        //byte[] results = www.downloadHandler.data;
-    //        //responseText.text = www.downloadHandler.text;
-    //    }
-    //    //WWWForm form = new WWWForm();
-    //    //// header 정의
-    //    //Dictionary<string, string> headers = form.headers;
-    //    //headers["test"] = "eeee";
-
-    //    //// path 정의
-    //    //string path = "/zone1/deviceEvents";
-    //    //string loginPath = "/user/management/login";
-
-    //    //// post body?
-    //    //form.AddField("id", "KSH");
-    //    //form.AddField("password", "111");
-    //    //byte[] rawFormData = form.data;
-
-    //    //WWW request = new WWW(URL + loginPath, form);
-
-    //    //Debug.Log(request.text);
-    //    //StartCoroutine(OnResponse(request));
-    //}
-
-    IEnumerator Upload()
-    {
-        WWWForm form = new WWWForm();
-        form.AddField("id", "KSH");
-        form.AddField("password", "111");
-
-        UnityWebRequest www = UnityWebRequest.Post("http://192.168.0.4:7080/heroes/user/management/login", form);
-        www.SetRequestHeader("content-type", "application/json;");
-        //UnityWebRequest www = UnityWebRequest.Get("http://192.168.0.4:7080/heroes/data/food/select");
-        yield return www.SendWebRequest();
-
-        if (www.isNetworkError || www.isHttpError)
-        {
-            Debug.Log(www.error);
-        }
-        else
-        {
-            // show result as text
-            Debug.Log("success!");
-            Response response = JsonUtility.FromJson<Response>(www.downloadHandler.text);
-
-            responseText.text = response.data.name;
-
-            //byte[] results = www.downloadHandler.data;
-            //responseText.text = www.downloadHandler.text;
-        }
-    }
 
     [Serializable]
     public class Login
@@ -142,27 +67,13 @@ public class API : MonoBehaviour {
         public string password;
     }
 
-    //IEnumerator Post(string url, string bodyJsonString)
-    //{
-    //    var request = new UnityWebRequest(url, "POST");
-    //    byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJsonString);
-    //    request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
-    //    request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-    //    request.SetRequestHeader("Content-Type", "application/json");
-
-    //    yield return request.Send();
-
-    //    Debug.Log("Status Code: " + request.responseCode);
-    //}
-
-    private UnityWebRequest makeWWW(string url, string bodyJsonString)
+    private UnityWebRequest makePOST(string url, string bodyJsonString)
     {
         var request = new UnityWebRequest(url, "POST");
-        //byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJsonString);
-        byte[] bodyRaw = new System.Text.UTF8Encoding().GetBytes(bodyJsonString);
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJsonString);
         request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
         request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Content-Type", "application/json; charset=utf-8");
 
         return request;
     }
@@ -174,8 +85,7 @@ public class API : MonoBehaviour {
 
     public void Request()
     {
-        //StartCoroutine(Upload());
-
+        // use json (안됨)
         Login login = new Login();
         login.id = "KSH";
         login.password = "111";
@@ -184,10 +94,10 @@ public class API : MonoBehaviour {
 
         Debug.Log(json);
 
-        //UnityWebRequest www = makeWWW("http://192.168.0.4:7080/heroes/user/management/login", json);
+        UnityWebRequest www = makePOST("http://192.168.0.4:7080/heroes/user/management/login", json);
 
-        UnityWebRequest www = UnityWebRequest.Post("http://192.168.0.4:7080/heroes/user/management/login", json);
-        www.SetRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        //UnityWebRequest www = UnityWebRequest.Post("http://192.168.0.4:7080/heroes/user/management/login", json);
+        //www.SetRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
         StartCoroutine(request(www));
 
@@ -199,10 +109,10 @@ public class API : MonoBehaviour {
         {
             // show result as text
             Debug.Log("success!");
-            Response response = JsonUtility.FromJson<Response>(www.downloadHandler.text);
+            //Response response = JsonUtility.FromJson<Response>(www.downloadHandler.text);
 
             //responseText.text = response.data.name;
-            responseText.text = www.downloadHandler.data.ToString();
+            responseText.text = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
             Debug.Log(www.downloadHandler.data.ToString());
             Debug.Log(www.downloadHandler.text);
         }
