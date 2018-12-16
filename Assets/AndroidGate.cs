d1e6b71346b6d0181e02140d648b4369d425909e
lmjing <lmjing1234@gmail.com>
2018-12-16 14:21:26 +0900

    android 연동 성공

---
+++ Assets/AndroidGate.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidGate : MonoBehaviour {
+
+    private AndroidJavaObject curActivity;
+    private AndroidJavaClass firstPluginJc;
+
+    public string FeedBack;
+
+    public string kidId = "test";
+
+    // Use this for initialization
+    void Start () {
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        curActivity = jc.GetStatic<AndroidJavaObject>("currentActivity");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // 인식한 밴드 id를 안드로이드에 보낸다
+    public void SetBandId(string id)
+    {
+        //Toast는 안드로이드의 UiThread를 사용하기때문에 
+        //UnityPlayerActivity UiThread를 호출하고, 다시 ShowToast를 호출합니다.
+        curActivity.Call("runOnUiThread", new AndroidJavaRunnable(() => {
+            curActivity.Call("setBandId", id);
+        }));
+    }
+
+    // 앱으로 부터 밴드를 연결시키고자 하는 아이의 id를 받아온다
+    public void SetKidId(string id)
+    {
+        kidId = id;
+    }
+
+    public void CallKidId()
+    {
+        curActivity.Call("setKidId");
+    }
+}
--- Assets/BandMarkerManager.cs
+++ Assets/BandMarkerManager.cs
@@ -1,29 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BandMarkerManager : MonoBehaviour {
     public TrackingObject obj_java_book;
     public TrackingObject obj_arvr_book;
+    public Text kid_id_text;
+
+    public AndroidGate gate;
 
     private void Awake()
     {
-        for (int i = 0; i < 10; i++)
-        {
-            GameObject g = new GameObject("abc" + i.ToString("D3"));
-        }
     }
 
     // Use this for initialization
     void Start () {
-        //GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("BookTag");
-
-        //foreach (GameObject go in gameObjects)
-        //{
-        //    Debug.Log(go + " is an active object " + go.GetInstanceID());
-        //}
-
-
+        gate.CallKidId();
     }
 	
 	// Update is called once per frame
@@ -33,14 +26,17 @@ public class BandMarkerManager : MonoBehaviour {
 
     private void OnGUI()
     {
-        // TODO : 인식되면 밴드 정보를 안드로이드로 넘겨준다
+        kid_id_text.text = gate.kidId;
+        // 인식되면 밴드 정보를 안드로이드로 넘겨준다
         if (obj_arvr_book.is_detected_)
         {
+            gate.SetBandId(obj_arvr_book.model_name);
             GUI.Button(new Rect(300, 300, 240, 120), obj_arvr_book.model_name + "mmmm");
         }
 
         if (obj_java_book.is_detected_)
         {
+            gate.SetBandId(obj_java_book.model_name);
             GUI.Button(new Rect(600, 300, 240, 120), obj_java_book.model_name);
         }
     }
